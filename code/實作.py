9f5709e54b5dad6fe55b7b0b2f629022374c0aeb
789sw <113761212+789sw@users.noreply.github.com>
2023-06-30 16:41:04 +0800

    practice

--- code/實作.py
+++ code/實作.py
@@ -1,2 +1,11 @@
-for i in range(3):
-    print(i)
+# x = input("Hello World")
+# print(x)
+if(x>y):
+    if(x>z):
+        print(x)
+elif(x<y):
+    print(y)
+else:
+    print(z)
+# for i in range(3):
+#     print(i)
